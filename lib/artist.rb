1d668fb155ae174b8ac995a12e963bf0baa49de1
Sam Markham <sam.e.markham@gmail.com>
2019-07-19 16:59:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,33 @@
+require 'pry'
+
 class Artist
   @@all = []
-  attr_accessor :name 
+  attr_accessor :name, :genres
 
   def initialize(name)
     @name = name
+    @genres = []
     @@all << self
   end
 
   def self.all
     @@all
   end
+
+  def new_song(song_name, genre)
+    song = Song.new(song_name, self, genre)
+  end
+
+  def songs
+    Song.all.select do |s|
+      s.artist == self
+    end
+  end
+
+  def genres
+    songs.map do |s|
+      s.genre
+    end
+  end
+
 end
